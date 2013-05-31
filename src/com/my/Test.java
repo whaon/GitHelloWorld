b5b44348c24810ca73d534eff4bfdf3b9728ef80
whaon <whaonchong@163.com>
2013-05-31 14:15:34 +0800

    GitHelloWorld first commit

---
+++ src/com/my/Test.java
@@ -0,0 +1,18 @@
+package com.my;
+
+public class Test {
+
+	/** 
+	 *  方法的简述. 
+	 * <p> 方法的详细说明第一行<br> 
+	 *  方法的详细说明第二行 
+	 * @param 参数
+	 * @exception 异常描述
+	 * @return 没有返回值 
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+
+}
